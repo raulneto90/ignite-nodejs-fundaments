4cbf8e03e67747edcc64e54f1a559f7937cfb33c
Raul Neto <raulneto90@gmail.com>
2021-03-09 12:28:18 -0300

    Updated routes to use an array of courses

--- src/index.js
+++ src/index.js
@@ -1,48 +1,76 @@
 const express = require('express');
+const {v4: uuid} = require('uuid');
 
 const app = express();
+app.use(express.json());
+
+const courses = [];
 
 app.get('/courses', (request, response) => {
-  return response.json([
-    'Curso 1',
-    'Curso 2',
-    'Curso 3'
-  ]);
+  return response.json(courses);
 });
 
 app.post('/courses', (request, response) => {
-  return response.json([
-    'Curso 1',
-    'Curso 2',
-    'Curso 3',
-    'Curso 4'
-  ]);
+  const { name } = request.body;
+
+  const course = {
+    id: uuid(),
+    name,
+  }
+
+  courses.push(course);
+
+  return response.json(course);
 });
 
 app.put('/courses/:id', (request, response) => {
-  return response.json([
-    'Curso 6',
-    'Curso 2',
-    'Curso 3',
-    'Curso 4'
-  ]);
+  const { id } = request.params;
+  const {name} = request.body;
+  
+  const courseIndex = courses.findIndex(course => course.id === id);
+
+  if(courseIndex < 0) {
+    return response.status(400).json({message: 'Course not found'});
+  }
+
+  courses[courseIndex] = {
+    id,
+    name
+  }
+
+  return response.json(courses[courseIndex]);
 });
 
 app.patch('/courses/:id', (request, response) => {
-  return response.json([
-    'Curso 6',
-    'Curso 7',
-    'Curso 3',
-    'Curso 4'
-  ]);
+  const { id } = request.params;
+  const {name} = request.body;
+  
+  const courseIndex = courses.findIndex(course => course.id === id);
+
+  if(courseIndex < 0) {
+    return response.status(400).json({message: 'Course not found'});
+  }
+
+  courses[courseIndex] = {
+    id,
+    name
+  }
+
+  return response.json(courses[courseIndex]);
 });
 
 app.delete('/courses/:id', (request, response) => {
-  return response.json([
-    'Curso 6',
-    'Curso 7',
-    'Curso 4'
-  ]);
+  const { id } = request.params;
+
+  const courseIndex = courses.findIndex(course => course.id === id);
+
+  if(courseIndex < 0) {
+    return response.status(400).json({message: 'Course not found'});
+  }
+
+  courses.splice(courseIndex, 1);
+
+  return response.json(courses);
 });
 
 
